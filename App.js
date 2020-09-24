aacefb07b527604157d3f91e611885364fdd7230
Yuri Samagaia <yurisamagaia@gmail.com>
2020-09-24 15:44:22 -0300

    install navigator & redux

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './src/components/header'
-
+ 
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
+ 
+import { Provider } from 'react-redux';
+ 
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './src/redux/reducers'
+ 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+ 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header title="FILMEFLIX" />
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <Provider store={store}>
+     <View style={styles.container}>
+       <NavigationContainer>
+         <StatusBar style="auto" />
+         <Header title="FILMEFLIX" />
+         <Routes />
+       </NavigationContainer>
+     </View>
+   </Provider>
+ );
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+ },
 });
